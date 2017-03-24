a0e9c0b4f35a5f0a58b045854c9c51012cc32b0c
Mario Viens <marviens@gmail.com>
2017-03-24 16:03:22 -0700

    updated email to 2 emails

--- controllers/requests.js
+++ controllers/requests.js
@@ -53,7 +53,7 @@ exports.postRequest = (req, res) => {
     if (err) { return next(err); }
 
   const mailOptions = {
-      to: 'marviens@gmail.com',
+      to: ['gabby.korcheva@binnersproject.org', 'marviens@gmail.com'],
       from: '"Binners Project" <info@binnersproject.org>',
       subject: `Binners Project - New Pickup! ${req.user.profile.name}`,
       html: `🎉🎉🎉<br/><br/>
