119a0563b3423cb02b01e16754bd19675a24686c
Mario Viens <marviens@gmail.com>
2017-03-24 13:34:39 -0700

    WIP faq dropdown

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,29 @@ $(document).ready(function() {
 
   $('#faq').on("click", () => {
     $('#about').slideToggle();
-  })
+  });
 
+  $('#nav-forward').on("click", () => {
+    $('#nav-back').removeClass("hidden");
+    let progress = Number($('.progress-bar').attr('aria-valuenow'));
+    progress += 25;
+    showPage(progress);
+  });
+
+    $('#nav-back').on("click", () => {
+    let progress = Number($('.progress-bar').attr('aria-valuenow'));
+    progress -= 25;
+    showPage(progress);
+  });
+
+  function showPage(progress) {
+    switch (progress) {
+      case 25:
+        $('.progress-bar span').text(`${progress} Complete`);
+        $('.progress-bar').attr('aria-valuenow', progress);
+      case 50:
+      case 75:
+      case 100:
+    }
+  }
 });
